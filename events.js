3e1d0d29aa22e483af0a0b746d5093483f567d12
Mitchell Blom <mitchellblom@gmail.com>
2017-02-28 21:14:49 -0600

    on line 09 of directions

--- events.js
+++ events.js
@@ -1,1 +1,15 @@
-alert("js works");
+//////////////////////// REFERENCES //////////////////////////
+
+var outputTarget = document.getElementById("output-target");
+
+//////////////////////// EVENT LISTENERS //////////////////////////
+
+document.addEventListener("click", function(e) {
+	writeToOutputTarget(e.innerText)
+});
+
+//////////////////////// FUNCTIONS //////////////////////////
+
+function writeToOutputTarget() {
+	outputTarget.innerHTML = 'You clicked on the ${text of the section} section'
+}
